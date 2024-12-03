ece11e8e7f96bf5ef303f666116fcbb316cbdcd4
EkanshGupta0618 <232349@kkit.ac.in>
2024-12-03 19:26:18 +0530

    new file:   app1/__init__.py 	new file:   app1/__pycache__/__init__.cpython-312.pyc 	new file:   app1/__pycache__/admin.cpython-312.pyc 	new file:   app1/__pycache__/apps.cpython-312.pyc 	new file:   app1/__pycache__/models.cpython-312.pyc 	new file:   app1/__pycache__/views.cpython-312.pyc 	new file:   app1/admin.py 	new file:   app1/apps.py 	new file:   app1/middleware.py 	new file:   app1/migrations/__init__.py 	new file:   app1/migrations/__pycache__/__init__.cpython-312.pyc 	new file:   app1/models.py 	new file:   app1/tests.py 	new file:   app1/views.py 	new file:   db.sqlite3 	new file:   manage.py 	new file:   registration/__init__.py 	new file:   registration/__pycache__/__init__.cpython-312.pyc 	new file:   registration/__pycache__/info.cpython-312.pyc 	new file:   registration/__pycache__/settings.cpython-312.pyc 	new file:   registration/__pycache__/urls.cpython-312.pyc 	new file:   registration/__pycache__/wsgi.cpython-312.pyc 	new file:   registration/asgi.py 	new file:   registration/info.py 	new file:   registration/settings.py 	new file:   registration/urls.py 	new file:   registration/wsgi.py 	new file:   static/css/forgot_password.css 	new file:   static/css/home.css 	new file:   static/css/login.css 	new file:   static/css/otp_fill.css 	new file:   static/css/signup.css 	new file:   static/images/1.jpeg 	new file:   static/images/2.jpeg 	new file:   static/images/3.jpeg 	new file:   static/images/3293465.jpg 	new file:   static/images/4.jpeg 	new file:   static/images/5.jpeg 	new file:   static/images/6.jpeg 	new file:   static/images/7.jpeg 	new file:   static/images/8.jpeg 	new file:   static/images/exclimation.png 	new file:   static/images/f1.png 	new file:   static/images/logo.png 	new file:   templates/dashboard.html 	new file:   templates/forgot_password.html 	new file:   templates/home.html 	new file:   templates/login.html 	new file:   templates/otp_fill.html 	new file:   templates/signup.html 	new file:   utils.py

---
+++ app1/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.conf import settings
+from django.contrib import messages
+# Create your views here.
+
+def home_page(request):
+    return render(request, 'home.html')
+
+
+
+#For login page of the website
+def login_page(request):
+    if request.method == 'POST':
+        # Get the post data
+        username = request.POST.get('username')
+        pass1 = request.POST.get('pass')
+        user=authenticate(request, username=username, password=pass1)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            #return HttpResponse('Invalid Credentials')
+            messages.error(request, 'Invalid Username or Password')
+    return render(request, 'login.html')
+
+
+
+
+#For signup page of the website
+def sign_up_page(request):
+    if request.method == 'POST':
+        # Get the post data
+        uname = request.POST.get('username')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+        # Check if the passwords match   
+        if pass1 != pass2:
+            messages.error(request, 'Passwords do not match')
+            return redirect('signup')
+        # Check if the username already exists
+        elif User.objects.filter(username=uname).exists():
+            messages.error(request, 'Username already exists')
+            return redirect('signup')
+        # check if the email already exists
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, 'Email already exists')
+            return redirect('signup')
+        else:
+            my_user = User.objects.create_user(uname, email, pass1)
+            my_user.save()
+            return redirect('login')  
+    return render(request, 'signup.html')
+
+
+def forgot_password(request): 
+    if request.method == 'POST':
+        # Get the post data
+        email = request.POST.get('email')
+        print ("Email: ", email)
+        if User.objects.filter(email=email).exists():
+            print('Email exists')
+            
+        return render(request, 'forgot_password.html')
+    return render(request, 'forgot_password.html')
+
+
+def otp_fill(request):
+    return render (request, 'otp_fill.html')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'dashboard.html')
