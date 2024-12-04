b2efdca9f7621d1df8a2a8b1f44de70a58821c73
EkanshGupta0618 <232349@kkit.ac.in>
2024-12-04 22:51:46 +0530

    modified:   registration/__pycache__/settings.cpython-312.pyc 	modified:   registration/__pycache__/urls.cpython-312.pyc 	modified:   registration/urls.py

--- registration/urls.py
+++ registration/urls.py
@@ -17,9 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app1 import views
-from django.views.static import serve
-from django.conf import settings
-from django.conf.urls import url
+
 
 
 
